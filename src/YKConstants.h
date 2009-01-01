08a864acd6ec966d93524249dda2fe7a4e489268
Patrick Thomson <ironswallow@gmail.com>
2009-01-01 18:49:57 -0500

    Imported a constants file which I will need in the future.

---
+++ src/YKConstants.h
@@ -0,0 +1,10 @@
+/*
+ *  YKConstants.h
+ *  YAMLKit
+ *
+ *  Created by Patrick Thomson on 1/1/09.
+ *  Copyright 2009 Patrick Thomson. All rights reserved.
+ *
+ */
+
+const NSString *YAMLKitErrorDomain = @"YKErrorDomain";
