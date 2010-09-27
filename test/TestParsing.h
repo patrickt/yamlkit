25d783b4f000f1ad8994a2f0e0e0b73e15041f03
Faustino E. Osuna <enrique.osuna@gmail.com>
2010-09-28 00:26:07 +0200

    Missing test declaration.

--- test/TestParsing.h
+++ test/TestParsing.h
@@ -14,6 +14,7 @@
 
 - (void)testVerySimpleLoadingFromFile;
 - (void)testVerySimpleStringParsing;
+- (void)testDigitPrefixedStringParsing;
 - (void)testModerateLoadingFromFile;
 - (void)testAutomaticIntegerCasting;
 - (void)testAutomaticBooleanCasting;
