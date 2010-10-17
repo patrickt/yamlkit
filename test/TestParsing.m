caf5d8fc7294a8b7c970ed80e25aa500ef22dbd4
Faustino E. Osuna <enrique.osuna@gmail.com>
2010-10-17 18:41:29 +0200

    Test that the parser returned an array of objects.

--- test/TestParsing.m
+++ test/TestParsing.m
@@ -55,6 +55,7 @@
 {
     [p readString:@"- 685230\n- +685_230\n- 02472256\n- 0x_0A_74_AE\n- 0b1010_0111_0100_1010_1110\n- 190:20:30\n"];
     NSArray *o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         STAssertTrue([value isKindOfClass:[NSNumber class]], @"was not a number");
         STAssertEqualObjects(value, [NSNumber numberWithInt:685230], @"incorrectly cast to NSNumber <%@(%@)>", NSStringFromClass([value class]), value);
@@ -65,6 +66,7 @@
 {
     [p readString:@"- 6.8523015e+5\n- 685.230_15e+03\n- 685_230.15\n- 190:20:30.15\n"];
     NSArray *o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         STAssertTrue([value isKindOfClass:[NSNumber class]], @"was not a number");
         STAssertEqualObjects(value, [NSNumber numberWithDouble:685230.15], @"incorrectly cast to NSNumber <%@(%@)>", NSStringFromClass([value class]), value);
@@ -75,6 +77,7 @@
 {
     [p readString:@"- true\n- True\n- TRUE\n- y\n- Y\n- Yes\n- YES\n- yes\n- on\n- On\n- ON\n"];
     NSArray *o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         if (CFGetTypeID(value) == CFBooleanGetTypeID()) {
             STAssertTrue([value boolValue], @"boolean value was not true");
@@ -84,6 +87,7 @@
     }
     [p readString:@"- false\n- False\n- FALSE\n- n\n- N\n- No\n- NO\n- off\n- Off\n- OFF\n"];
     o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         if (CFGetTypeID(value) == CFBooleanGetTypeID()) {
             STAssertFalse([value boolValue], @"boolean value was not false");
@@ -97,6 +101,7 @@
 {
     [p readString:@"- null\n- Null\n- NULL\n- ~\n- \n"];
     NSArray *o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         STAssertEqualObjects(value, [NSNull null], @"incorrectly cast to NSNull <%@(%@)>", NSStringFromClass([value class]), value);
     }
@@ -107,6 +112,7 @@
     [p readString:@"- 2001-12-14t21:59:43.10-05:00\n- 2001-12-14 21:59:43.10 -5\n"];
     NSDate *date = [[NSDate dateWithString:@"2001-12-14 23:29:43 +0100"] dateByAddingTimeInterval:0.1];
     NSArray *o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         STAssertEqualObjects(value, date, @"incorrectly cast to NSDate <%@(%@)>", NSStringFromClass([value class]), value);
     }
@@ -114,6 +120,7 @@
     [p readString:@"- 2001-12-15T02:59:43.1Z\n- 2001-12-15 2:59:43.10\n"];
     date = [[NSDate dateWithString:@"2001-12-15 03:59:43 +0100"] dateByAddingTimeInterval:0.1];
     o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         STAssertEqualObjects(value, date, @"incorrectly cast to NSDate <%@(%@)>", NSStringFromClass([value class]), value);
     }
@@ -121,6 +128,7 @@
     [p readString:@"- 2001-12-14\n"];
     date = [NSDate dateWithString:@"2001-12-14 01:00:00 +0100"];
     o = [[p parse] objectAtIndex:0];
+    STAssertTrue([o count], @"parser returned nothing.");
     for (id value in o) {
         STAssertEqualObjects(value, date, @"incorrectly cast to NSDate <%@(%@)>", NSStringFromClass([value class]), value);
     }
